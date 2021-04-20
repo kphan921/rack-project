f8470fd7f4356854dc1cd48432325ba12694eb05
Tri <triphan921@gmail.com>
2021-04-20 09:15:03 -0700

    added components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Redirect, Switch, Route } from 'react-router-dom'
+import NotFound from './components/NotFound.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    brands: [],
+  };
+
+  componentDidMount() {
+    console.log('Hi')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>
+            app
+          </h2>
+        </header>
+        <main>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route component={NotFound} />
+          </Switch>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
